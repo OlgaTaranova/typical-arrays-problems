f3c3f88d2c1f3634beed88eaea63d71d0c08e965
Olga Taranova <olka_olkataranova@mail.ru>
2020-10-01 21:40:22 +0300

    feat: implement function 'max'

--- src/index.js
+++ src/index.js
@@ -12,7 +12,15 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) == false || array.length == 0) return 0;
+  let max = array[0];
   
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
