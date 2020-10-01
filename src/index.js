d7bacc840ee8aaf55da8aaca464c5046be602fcc
Olga Taranova <olka_olkataranova@mail.ru>
2020-10-01 21:41:30 +0300

    feat: implement function 'avg'

--- src/index.js
+++ src/index.js
@@ -24,5 +24,11 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
- 
+  if (Array.isArray(array) == false || array.length == 0) return 0;
+  let sum = 0;
+  
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return +(sum/array.length).toFixed(2);
 }
